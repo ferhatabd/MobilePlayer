5e3d262e93654e5a63d0d1b7defcbc5cee555933
Ferhat Abdullahoglu <abdullahoglu@sabanciuniv.edu>
2020-05-31 23:32:37 +0300

    aws implementations

--- Sources/MobilePlayer/Views/PlayerView.swift
+++ Sources/MobilePlayer/Views/PlayerView.swift
@@ -70,7 +70,6 @@ public class PlayerView: UIView {
     public var url: URL? {
         didSet {
             if let _url = url {
-                let _player = AVPlayer(url: _url)
                 _player.allowsExternalPlayback = true
                 player = _player
             }
